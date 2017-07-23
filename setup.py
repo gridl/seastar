c29d57c2e7bec7f624f341150630d487ea4e917f
Ole Weidner <ole.c.weidner@gmail.com>
2017-07-23 13:38:59 +0200

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -25,7 +25,7 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.1.0',
 
-    description='A Telemetry Data Framework for HPC Environments',
+    description='A telemetry framework for HPC systems and applications',
     long_description=long_description,
 
     # The project's main homepage.
@@ -36,7 +36,7 @@ setup(
     author_email='ole.weidner@ed.ac.uk',
 
     # Choose your license
-    license='MIT',
+    license='GPL-2.0',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -64,7 +64,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='hpc telemetry mpi openmp',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -78,7 +78,12 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['flask', 'graphene'],
+    install_requires=[
+        'graphene-sqlalchemy>=1.0',
+        'SQLAlchemy==1.0.11',
+        'Flask==0.10.1',
+        'Flask-GraphQL==1.3.0',
+        'click==6.7'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -92,22 +97,24 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'seastar': ['package_data.dat'],
-    },
+
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+
+    # data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'seastar=seastar:main',
+            'seastar-apiserver=seastar.apiserver:main',
         ],
     },
 )
